8f7e9eb282e83e2bed21f63048a679d6a7f319da
Carapacik <carapacik@gmail.com>
2021-12-24 00:57:23 +0300

    Upd, but not work

--- Assets/Scripts/VictoryController.cs
+++ Assets/Scripts/VictoryController.cs
@@ -1,25 +1,26 @@
 using System.Collections;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 using TMPro;
 using UnityEngine;
 
 public class VictoryController : MonoBehaviour
 {
-    private static Answers _answers;
     private static Tasks _tasks;
 
     [SerializeField] private GameObject taskText;
     [SerializeField] private GameObject levelText;
     [SerializeField] private GameObject livesCountText;
     [SerializeField] private GameObject gameOverText;
+    [SerializeField] private GameObject victoryText;
     [SerializeField] private int livesCount = 5;
 
+    private int _currentAnswer;
     private int _currentLevelNumber;
     private int _currentTaskNumber;
     private GameObject[] _ships;
 
     public VictoryController()
     {
-        _answers = new Answers();
         _tasks = new Tasks();
     }
 
@@ -38,7 +39,7 @@ public class VictoryController : MonoBehaviour
     private void Update()
     {
         if (livesCount >= 0) return;
-        StartCoroutine(Wait5Seconds());
+        StartCoroutine(ShowGameOver());
         _currentTaskNumber = 0;
         _currentLevelNumber = 0;
         livesCount = 5;
@@ -50,7 +51,7 @@ public class VictoryController : MonoBehaviour
 
     public bool CheckAnswer(int number)
     {
-        if (number == _answers.AllAnswers[_currentLevelNumber][_currentTaskNumber]) return true;
+        if (number == _currentAnswer) return true;
 
         livesCount--;
         livesCountText.transform.GetComponent<TextMeshProUGUI>().text = $"Lives: {livesCount}";
@@ -63,7 +64,12 @@ public class VictoryController : MonoBehaviour
     {
         switch (start)
         {
+            case false when _tasks.AllTasks.Count == _currentLevelNumber + 1:
+                StartCoroutine(ShowVictory(10));
+                Start();
+                break;
             case false when _tasks.AllTasks[_currentLevelNumber].Count == _currentTaskNumber + 1:
+                StartCoroutine(ShowVictory());
                 _currentLevelNumber++;
                 levelText.transform.GetComponent<TextMeshProUGUI>().text =
                     $"Level: {_currentLevelNumber + 1}";
@@ -76,21 +82,30 @@ public class VictoryController : MonoBehaviour
         var currentCorrect = Random.Range(0, _ships.Length);
         for (var i = 0; i < _ships.Length; i++)
         {
-            var answer = _answers.AllAnswers[_currentLevelNumber][_currentTaskNumber];
+            var expression = _tasks.AllTasks[_currentLevelNumber][_currentTaskNumber];
+            _currentAnswer = CSharpScript.EvaluateAsync<int>(expression).Result;
+
             _ships[i].gameObject.GetComponent<Ship>().number =
                 i == currentCorrect
-                    ? answer
-                    : answer + Random.Range(-5, 15);
+                    ? _currentAnswer
+                    : _currentAnswer + Random.Range(-5, 10);
         }
 
         taskText.transform.GetComponent<TextMeshProUGUI>().text =
             _tasks.AllTasks[_currentLevelNumber][_currentTaskNumber];
     }
 
-    private IEnumerator Wait5Seconds()
+    private IEnumerator ShowVictory(int sec = 5)
+    {
+        victoryText.SetActive(true);
+        yield return new WaitForSeconds(sec);
+        victoryText.SetActive(false);
+    }
+
+    private IEnumerator ShowGameOver(int sec = 5)
     {
         gameOverText.SetActive(true);
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(sec);
         gameOverText.SetActive(false);
     }
 }
